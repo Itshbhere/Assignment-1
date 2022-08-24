c665457aed53c6e94f7ddc15a2fbffa4c17bebf0
Hafiz Burhan <larsalaxsanderson@gmail.com>
2022-08-24 15:07:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment-1
+ Basic code of java script
---
+++ assignment1.js
@@ -0,0 +1,47 @@
+console.clear();
+//clears the console
+alert("Welcome to the page ");
+//Displays an alert message on the bar
+console.time();
+//Starts default timer of the operation
+alert(2*38*3);
+//Displays the value 
+alert("This is the first page \n now moving to the next line \n Thank You!!");
+//Displays message with line break
+console.log("WELCOME TO THE BLOCKCHAIN");
+//Message on console
+console.log(1);
+//Displaying number on console
+console.log(null);
+//displaying null value on console
+console.log(false)
+//displaying bool type on console
+console.log([69,79,89]);
+//dislaying array on console
+console.log({firstName:"Hafiz Burhan", age : 19 , degree :"Bachelors in Computer sciences" });
+//displaying object on console
+console.table({my_name:"Hafiz Burhan", my_age : 19 , my_degree :"Bachelors in Computer sciences" });
+//Displaying table on console
+console.warn("Warning proceed on your own risk");
+//Warning message on console 
+console.timeEnd();
+//Dispalys the time taklen for the above operations
+document.write("THIS is magical \n");
+//Write this on the on the webpage
+document.write([34,9,72,2]);
+//Write array on document
+var Numbertype= 68;
+console.log("The first variable is an integer " , Numbertype);
+//Initializing a change able Variable of integer type and the n diplaying thios on console
+var floatType =69.9;
+console.log("The float variable is ", floatType);
+//Initializing a change able Variable of float type and the n diplaying thios on console
+let booleanType = true;
+console.log("The boolean type is ", booleanType);
+//Initializing a change able Variable of Boolean type and the n diplaying thios on console
+let nullType = null;
+console.log("The null type is ", nullType);
+//Initializing a change able Variable of null type and the n diplaying thios on console
+const unchangeable=2;
+console.log("The constant value is ", unchangeable);
+////Initializing a Constant  of integer type and the n diplaying thios on console
